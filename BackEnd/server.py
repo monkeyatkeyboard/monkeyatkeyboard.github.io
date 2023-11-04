d60381dc50f1dd3a2a568e19fd6f3ec39fe72c25
Jonathan Vigil <jvigil1738@gmail.com>
2023-11-04 16:40:11 -0700

    fixed navigation bar link routes

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -24,4 +24,5 @@ def get_output():
     )
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
+    # serve(app, host="0.0.0.0", port=8000)
