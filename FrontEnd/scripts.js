baa5a905c83db6fc82b70075bb9929070e76ab43
Shoheicode <149898948+monkeyatkeyboard@users.noreply.github.com>
2023-11-04 15:56:27 -0700

    Added a couple of details/organized it a bit

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -7,4 +7,29 @@
 //     preventDefault();
 // //     const response = await fetch('fjksl;fdsjfl the HTTPS route for backend') //change when api route obtained
 // })
-alert("hi");
+window.onload = function(){
+    
+    var a = document.getElementById("myDiv")
+    if(a){
+        a.addEventListener("mousemove", function(event) {
+            myFunction(event);
+        });   
+    }
+}
+
+document.getElementById("myDiv").addEventListener("mousemove", function(event) {
+    myFunction(event);
+});  
+  
+  function myFunction(e) {
+    let img = document.getElementById("MONKEY");
+    let offsetX = e.clientX;
+    let offsetY =  e.clientY;
+
+    let coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("text-input").innerHTML = coor;
+
+    img.style.left = offsetX + "px";
+    img.style.top= offsetY + "px";
+
+ }
